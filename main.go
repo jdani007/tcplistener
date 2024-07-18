8ae3423fe388fba7cd6530074c45240cbb007d7e
James Daniels <james.daniels@nokia.com>
2024-07-17 21:08:08 -0500

    go fmt

--- main.go
+++ main.go
@@ -7,38 +7,40 @@ import (
 )
 
 func main() {
-    done := make(chan bool)
+	done := make(chan bool)
 
-    flag.Parse()
-    if len(flag.Args()) == 1 {
-        fmt.Print("Listening on port: ")
-    } else {
-        fmt.Print("Listening on ports: ")
-    }
-    for _, port := range flag.Args() {
-        fmt.Printf("%v ", port)
-        go listen(port, done)
-    }
-    <-done
+	flag.Parse()
+
+	if len(flag.Args()) == 1 {
+		fmt.Print("Listening on port: ")
+	} else {
+		fmt.Print("Listening on ports: ")
+	}
+
+	for _, port := range flag.Args() {
+		fmt.Printf("%v ", port)
+		go listen(port, done)
+	}
+
+	<-done
 }
 
-
 func listen(port string, end chan bool) {
-    listener, err := net.Listen("tcp", ":" + port)
-    if err != nil {
-        fmt.Println("Error creating listener:", err)
-        end <- true
-        return
-    }
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Error creating listener:", err)
+		end <- true
+		return
+	}
 
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Println("Error accepting connection:", err)
-            end <- true
-            return
-        }
-        conn.Write([]byte("Port is open. Goodbye!\n"))
-        conn.Close()
-    }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			end <- true
+			return
+		}
+		conn.Write([]byte("Port is open. Goodbye!\n"))
+		conn.Close()
+	}
 }
